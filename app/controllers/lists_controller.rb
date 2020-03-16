c4765d7b2302cf72eae7c04b730ad935f166fb7e
juancarlosasensio <jasensio@ufm.edu>
2020-03-16 19:49:14 +0000

    Seven CRUD actions for ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,45 @@
 class ListsController < ApplicationController
+  before_action :find_list, only: [:show, :edit, :update, :destroy  ]
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @board = Board.find(params[:board_id])
+    @list = List.new(list_params)
+    @list.board_id = @board.id
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    @list.update(list_params)
+    redirect_to
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
+  end
 end
