7d571f3cf60661a938b79ac5f2e4b38b022449d2
juancarlosasensio <jasensio@ufm.edu>
2020-03-17 11:46:10 +0000

    Annotate models and routes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: lists
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :bigint           not null
+#
+# Indexes
+#
+#  index_lists_on_board_id  (board_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
 class List < ApplicationRecord
   belongs_to :board
   validates :name, presence: true
